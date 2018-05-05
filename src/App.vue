fb7bfa4852f4b3e6100b49dcfb8b0020b66649b6
daizeming <531735913@qq.com>
2018-05-05 21:55:57 +0800

    3.2基本骨架搭建完成

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
     <div id="app">
         <m-header></m-header>
+        <header-tab></header-tab>
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
 import MHeader from "@/components/MHeader/MHeader";
+import HeaderTab from "@/components/HeaderTab/HeaderTab";
 
 export default {
 	name: "App",
 		components: {
-			MHeader
+            MHeader,
+            HeaderTab
 	}
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,37 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
+import RecommendPage from '@/components/RecommendPage/RecommendPage'
+import SingerPage from '@/components/SingerPage/SingerPage'
+import RankPage from '@/components/RankPage/RankPage'
+import SearchPage from '@/components/SearchPage/SearchPage'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'HelloWorld',
-    //   component: HelloWorld
-    // }
+    {
+      path: '/',
+      redirect: 'recommend',
+    },
+    {
+        path: '/recommend',
+        name: 'recommend',
+        component: RecommendPage
+    },
+    {
+        path: '/singer',
+        name: 'singer',
+        component: SingerPage
+    },
+    {
+        path: '/rank',
+        name: 'rank',
+        component: RankPage
+    },
+    {
+        path: '/search',
+        name: 'search',
+        component: SearchPage
+    }
   ]
 })
